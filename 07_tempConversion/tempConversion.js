7b4dcd689e9a7ff0184a22c7f8ce186ecf1c7812
timeades <tim.eades@mac.com>
2023-01-01 20:44:45 +0000

    soultions to 6 & 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+  let celcius = Math.round(((farenheit - 32) * (5/9)) * 10) / 10;
+  return celcius; 
 };
+console.log(convertToCelsius())
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let farenheit = Math.round(((celcius * 9/5) + 32) * 10) / 10;
+  return farenheit;
 };
 
 // Do not edit below this line
@@ -9,3 +14,4 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
