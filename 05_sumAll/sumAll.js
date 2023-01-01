d6d1ed77160ec97b2b42d26f617ead58565735ad
timeades <tim.eades@mac.com>
2023-01-01 18:22:23 +0000

    solution for number 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,20 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+    if (typeof (num1) !== 'number' || typeof (num2) !== 'number' || num1 < 0 || num2 < 0) return 'ERROR';
+    else {
+        let sum = 0;
 
-};
+        let temp;
+        if (num1 > num2) {
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
 
 // Do not edit below this line
 module.exports = sumAll;
