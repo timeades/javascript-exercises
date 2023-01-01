578fde1c3b10eef49820b90124e29979befd1020
timeades <tim.eades@mac.com>
2023-01-01 17:01:15 +0000

    exercise 1, 2 & 3 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function (string, number) {
+    let repeat = '';
+    if (number < 0) {
+        return ("ERROR");
+    } else if (number == 0) {
+        return repeat;
+    } else {
+        for (let index = 0; index < number; index++) {
+            repeat = repeat + string;
+        }
+        return repeat;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
