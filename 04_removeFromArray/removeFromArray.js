3d43206a636abec6b1e7531f44542411d672dd62
timeades <tim.eades@mac.com>
2023-01-01 17:52:23 +0000

    solution for number 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args) {
+    let array = args[0];
 
+    const newArray = [];
+
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
