348ed3af0af80e03ec567a52a02fd7d9b9806527
timeades <tim.eades@mac.com>
2023-01-03 13:47:59 +0000

    finally finished exercise 8 - factorials

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -11,16 +11,23 @@ const sum = function(array) {
 };
 
 const multiply = function(array) {
-
+  return array.reduce((a, b) => a * b, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num < 1) {
+    return 1
+  } else if(num == 0) {
+    return 1;
+  } else {
+    return (num * factorial(num -1));
+  }
 };
+
 
 // Do not edit below this line
 module.exports = {
