2b53091d461bdefe84e69f0d79c8781866faf239
timeades <tim.eades@mac.com>
2023-01-03 15:50:53 +0000

    exercise 9 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const re = /[^A-Za-z0–9]/g;
+    const lowRegStr = str.toLowerCase().replace(re, ''); //stripping all the spaces from the string
+    const reverseStr = lowRegStr.split('').reverse().join(''); 
+    return reverseStr === lowRegStr;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
